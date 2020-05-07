ac68be55219cfe744ad6937b1245e686faccfb97
dtkachov <tkachov@localhost.localdomain>
2020-05-07 14:29:59 +0300

    Hiden APi KEY

--- src/App.js
+++ src/App.js
@@ -1,71 +1,84 @@
-import React, { Component, Fragment } from 'react';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import axios from 'axios' 
-import {USER_LOADED} from './actions/types.js'
+import React, { Component, Fragment } from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import axios from "axios";
+import { USER_LOADED } from "./actions/types.js";
 
-import { Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-import Header from './components/layout/Header'
-import Products from './components/dashboard/Products'
-import CreateProduct from './components/dashboard/CreateProduct'
-import ChangeProduct from './components/dashboard/ChangeProduct.js'
-import Image from './components/dashboard/ImageSet.js'
-import Login from './components/accounts/Login'
-import Alert from './components/layout/Alerts.js'
+import Header from "./components/layout/Header";
+import Products from "./components/dashboard/Products";
+import CreateProduct from "./components/dashboard/CreateProduct";
+import ChangeProduct from "./components/dashboard/ChangeProduct.js";
+import Image from "./components/dashboard/ImageSet.js";
+import Login from "./components/accounts/Login";
+import Alert from "./components/layout/Alerts.js";
 
-import Register from './components/accounts/Register'
-import PrivateRoute from './components/common/PrivateRoute'
+import Register from "./components/accounts/Register";
+import PrivateRoute from "./components/common/PrivateRoute";
 
-import { Provider } from 'react-redux'
-import { logOut  } from './actions/auth.js'
-import store from './store.js'
-import jwtDecode from 'jwt-decode'
+import { Provider } from "react-redux";
+import { logOut } from "./actions/auth.js";
+import store from "./store.js";
+import jwtDecode from "jwt-decode";
 
 //Alert Options
 const alertOptions = {
-    timeout: 3000,
-    position: 'top center'
-}
+  timeout: 3000,
+  position: "top center",
+};
 
-const token = localStorage.FBIdToken
+const token = localStorage.FBIdToken;
 if (token) {
-    const decodedToken = jwtDecode(token)
-    console.log(decodedToken)
-    if(decodedToken.exp * 1000 < Date.now()) {
-        store.dispatch(logOut())
-        window.location.href = '/login'
-    } else {
-        store.dispatch({type: USER_LOADED})
-        axios.defaults.headers.common['Athorization'] = token
-    }
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logOut());
+    window.location.href = "/login";
+  } else {
+    store.dispatch({ type: USER_LOADED });
+    axios.defaults.headers.common["Athorization"] = token;
+  }
 }
 
-class App extends Component{
-    render () { 
-      return (
-          <Provider store={store}>
-           <AlertProvider template={AlertTemplate} {...alertOptions}>
-            <Router>
-                 <Fragment>
-                     <Header />
-                        <Alert />
-                        <div className="container">
-                            <Switch>
-                                <PrivateRoute exact path="/" component={Products} />
-                                <PrivateRoute exact path="/createProduct" component={CreateProduct} />
-                                <PrivateRoute exact path="/changeProduct"  component={ChangeProduct} />
-                                <PrivateRoute exact path="/setImage"  component={Image} />
-                                <Route exact path="/register" component={Register} />
-                                <Route exact path="/login" component={Login} />
-                            </Switch>
-                        </div>  
-                 </Fragment>
-             </Router>
-          </AlertProvider>
-        </Provider>
-      ) 
-    }
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alert />
+              <div className="container">
+                <Switch>
+                  <PrivateRoute exact path="/" component={Products} />
+                  <PrivateRoute
+                    exact
+                    path="/createProduct"
+                    component={CreateProduct}
+                  />
+                  <PrivateRoute
+                    exact
+                    path="/changeProduct"
+                    component={ChangeProduct}
+                  />
+                  <PrivateRoute exact path="/setImage" component={Image} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertProvider>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/components/dashboard/Products.js
+++ src/components/dashboard/Products.js
@@ -1,52 +1,50 @@
-import React, {  useEffect } from 'react'
-import { connect }  from 'react-redux';
-import PropTypes from 'prop-types'
-import { getAllProducts, deleteOneProduct } from '../../actions/products.js'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getAllProducts, deleteOneProduct } from "../../actions/products.js";
 
-import Card from './ProductCard.js'
+import Card from "./ProductCard.js";
 
-function Products (props) {
+function Products(props) {
+  const style = {
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+  };
 
+  useEffect(() => {
+    props.getAllProducts();
+  }, []);
 
-    const style = {
-        display: 'flex',
-        justifyContent: 'space-around',
-        flexWrap: 'wrap', 
-    }
-
-    useEffect( () => {
-            props.getAllProducts()
-        }, [])
-    let card = props.products.map( product =>{ 
-        return (
-            <Card key={product.productId}
-                productId={product.productId}
-                deleteOneProduct={props.deleteOneProduct}
-                title={product.title}
-                imageUrl={product.imageUrl}
-                description={product.description}
-                discount={product.discount}
-                discountDate={product.discountDate}
-                price={product.price}
-                />
-            )
-        })
-
+  let card = props.products.map((product) => {
     return (
-        <div style={style}>
-          {card}
-        </div>
-        )
+      <Card
+        key={product.productId}
+        productId={product.productId}
+        deleteOneProduct={props.deleteOneProduct}
+        title={product.title}
+        imageUrl={product.imageUrl}
+        description={product.description}
+        discount={product.discount}
+        discountDate={product.discountDate}
+        price={product.price}
+      />
+    );
+  });
+
+  return <div style={style}>{card}</div>;
 }
 
 const propTypes = {
-        products:PropTypes.array.isRequired,
-        getAllProducts:PropTypes.func.isRequired,
-        deleteOneProduct:PropTypes.func.isRequired,
-    }
+  products: PropTypes.array.isRequired,
+  getAllProducts: PropTypes.func.isRequired,
+  deleteOneProduct: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-    products: state.products.products
-})
+const mapStateToProps = (state) => ({
+  products: state.products.products,
+});
 
-export default connect(mapStateToProps, { getAllProducts, deleteOneProduct })(Products)
+export default connect(mapStateToProps, { getAllProducts, deleteOneProduct })(
+  Products
+);
